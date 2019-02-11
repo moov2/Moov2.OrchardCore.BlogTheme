d16642249a313e652a6bff78f4dcc65c218d682a
Peter Keating <peter@moov2.com>
2019-02-11 20:28:56 +0000

    Create initial theme using Moov2.OrchardCore.ThemeBoilerplate

---
+++ Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "Blog Theme",
+    Author = "Moov2 Ltd.",
+    Website = "https://orchardcore.readthedocs.io/en/latest/",
+    Version = "0.0.1",
+    Description = "Theme for simple blog website"
+)]
