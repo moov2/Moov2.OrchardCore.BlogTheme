9e7639de4999ded988a81eff0892a759f98a05e3
Peter Keating <peter@moov2.com>
2019-02-11 21:26:33 +0000

    Set website in manifest to GitHub repository

--- Manifest.cs
+++ Manifest.cs
@@ -3,7 +3,7 @@ using OrchardCore.DisplayManagement.Manifest;
 [assembly: Theme(
     Name = "Blog Theme",
     Author = "Moov2 Ltd.",
-    Website = "https://orchardcore.readthedocs.io/en/latest/",
+    Website = "https://github.com/moov2/Moov2.OrchardCore.BlogTheme",
     Version = "0.0.1",
     Description = "Theme for simple blog website"
 )]
